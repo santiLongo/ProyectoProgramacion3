08693c30912314d557c1fe891d373d938ec0b3dc
Santiago Longo <santiagomaxilongo@gmail.com>
2025-10-07 21:25:51 -0300

    Subo el cambio del home

---
+++ Front/src/components/Header/Header.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {
+  AntDesignOutlined,
+  AppstoreOutlined,
+  CommentOutlined,
+  HolderOutlined,
+  PushpinOutlined,
+  SettingOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
+import { type MenuProps, Menu, Avatar } from "antd";
+import { Header } from "antd/es/layout/layout";
+
+type MenuItem = Required<MenuProps>["items"][number];
+
+const items: MenuItem[] = [
+  { key: "1", icon: <AppstoreOutlined />, label: "Inicio" },
+  { key: "2", icon: <CommentOutlined />, label: "Mensajes" },
+  { key: "3", icon: <PushpinOutlined />, label: "Publicacion" },
+  { key: "4", icon: <SettingOutlined />, label: "Ajustes" },
+  { key: "5", icon: <UserOutlined />, label: "Mi Perfil" },
+];
+
+const HeaderComponent: React.FC = () => {
+  return (
+    <>
+      <Header
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+          height: "100%",
+          maxHeight: 120,
+          display: "flex",
+          alignItems: "center",
+          background: "var(--primary-color)",
+        }}
+      >
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+          items={items}
+          style={{ flex: 1, minWidth: 0, background: "transparent" }}
+          expandIcon={<HolderOutlined />}
+        />
+        <div
+          className="navbar-right"
+          style={{
+            justifyContent: "flex-end",
+            marginRight: 20,
+            cursor: "pointer",
+          }}
+        >
+          <Avatar
+            onClick={() => console.log("Te llevo al home")}
+            size="large"
+            src="https://api.dicebear.com/8.x/adventurer/svg?seed=RoboDev"
+            icon={<AntDesignOutlined />}
+          />
+        </div>
+      </Header>
+    </>
+  );
+};
+
+export default HeaderComponent;
--- Front/src/modules/home/Home.tsx
+++ Front/src/modules/home/Home.tsx
@@ -1,108 +1,34 @@
-import React from 'react';
+import React from "react";
 import {
-  AntDesignOutlined,
-  AppstoreOutlined,
-  CommentOutlined,
-  PushpinOutlined,
-  SearchOutlined,
-  SettingOutlined,
-  UserOutlined
-} from '@ant-design/icons';
-import { Avatar, Card, Col, Layout, Menu, Row, Segmented, Select, type MenuProps } from 'antd';
-import { Content } from 'antd/es/layout/layout';
-import './home.css';
-import type { SegmentedLabeledOption } from 'antd/es/segmented';
+  Layout,
+} from "antd";
+import { Content } from "antd/es/layout/layout";
+import "./home.css";
+import HeaderComponent from "../../components/Header/Header";
 
-type MenuItem = Required<MenuProps>['items'][number];
 
-const items: MenuItem[] = [
-  { key: '1', icon: <AppstoreOutlined />, label: 'Inicio' },
-  { key: '2', icon: <CommentOutlined />, label: 'Mensajes' },
-  { key: '3', icon: <PushpinOutlined />, label: 'Publicacion' },
-  { key: '4', icon: <UserOutlined />, label: 'Mi Perfil' },
-  { key: '5', icon: <SettingOutlined />, label: 'Ajustes' },
-];
-
-const options: SegmentedLabeledOption<number>[] = [
-  { 
-    label: "Para ti",
-    value: 1
-  },
-  { 
-    label: "Siguiendo",
-    value: 2
-  },
-]
 
 const Home: React.FC = () => {
   const soyEmprendedor = false;
 
-
   return (
     <>
-      <Layout style={{ minHeight: '100vh', display: 'flex', flexDirection: 'row' }}>
-        <Menu
-            defaultSelectedKeys={["1"]}
-            mode="inline"
-            theme="dark"
-            inlineCollapsed={true}
-            items={items}
-          />
-        <Layout style={{ backgroundColor: 'var(--primary-color)', borderLeft: '1px solid var(--secondary-color)', display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', marginLeft: 191}}>
-          <Content style={{ border: 'none', backgroundColor: 'var(--primary-color)' }}>
-            <Row>
-              <Segmented
-              defaultValue={1}
-              size='large'
-              className='custom-segmented'
-              options={options}
-              ></Segmented>
-            </Row>
-          </Content>
-          <Content className='content-publicacion' hidden={soyEmprendedor}>
-            <Card className='custom-card'>
-              <Card.Meta
-                className='custom-card-meta'
-                avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />}
-                title="Problematica Ejemplo"
-                description='descripcion de la problematica ejemplo'
-              />
-            </Card>
-            <Card className='custom-card'>
-              <Card.Meta
-                className='custom-card-meta'
-                avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />}
-                title="Problematica Ejemplo"
-                description='descripcion de la problematica ejemplo'
-              />
-            </Card>
-            <Card className='custom-card'>
-              <Card.Meta
-                className='custom-card-meta'
-                avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />}
-                title="Problematica Ejemplo"
-                description='descripcion de la problematica ejemplo'
-              />
-            </Card>
-            <Card className='custom-card'>
-              <Card.Meta
-                className='custom-card-meta'
-                avatar={<Avatar src="https://api.dicebear.com/7.x/miniavs/svg?seed=1" />}
-                title="Problematica Ejemplo"
-                description='descripcion de la problematica ejemplo'
-              />
-            </Card>
-          </Content>
-        </Layout>
-        <Layout style={{ maxWidth: 400, backgroundColor: 'var(--primary-color)', borderLeft: '1px solid var(--secondary-color)' }}>
-          <Select
-            className='custom-select'
-            style={{ width: '95%', margin: '16px', backgroundColor: 'black', color: 'white' }} 
-            // suffixIcon={<SearchOutlined/>}
-            placeholder="Buscar..."
-            showSearch={true}
-          ></Select>
-        </Layout>
+      <Layout style={{ height: " 100vh " }}>
+        <HeaderComponent></HeaderComponent>
+        <Content style={{ padding: "0 48px" }}>
+          <div
+            style={{
+              marginTop: 30,
+              padding: 24,
+              height: "100%",
+              maxHeight: 600,
+              background: "var(--text-color)",
+              borderRadius: "var(--secundary-color)",
+            }}
+          >
+            Content
+          </div>
+        </Content>
       </Layout>
     </>
   );
